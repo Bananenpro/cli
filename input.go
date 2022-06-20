1988e500ecee8b69d93d4165de1f8329e3dcab86
Julian Hofmann <julian.hofmann2805@gmail.com>
2022-06-20 22:02:29 +0200

    Initial commit

---
+++ input.go
@@ -0,0 +1,67 @@
+package cli
+
+import (
+	"errors"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
+)
+
+var (
+	// ErrCanceled indicates that the user has interrupted the programm, e.g. by using Ctrl + C.
+	ErrCanceled = errors.New("Canceled.")
+)
+
+// Input ask the user to input a line of text.
+func Input(prompt string, validators ...Validator) (string, error) {
+	var result string
+	err := survey.AskOne(&survey.Input{
+		Message: prompt,
+	}, &result, survey.WithValidator(survey.Required))
+	if err == terminal.InterruptErr {
+		err = ErrCanceled
+	}
+	return result, err
+}
+
+// YesNo asks the user a yes/no question.
+func YesNo(question string, defaultValue bool) (yes bool, err error) {
+	err = survey.AskOne(&survey.Confirm{
+		Message: question,
+		Default: defaultValue,
+	}, &yes, survey.WithValidator(survey.Required))
+	if err == terminal.InterruptErr {
+		err = ErrCanceled
+	}
+	return yes, err
+}
+
+// Select asks the user to select on of many options. It returns the index of the chosen option.
+func Select(msg string, options []string) (int, error) {
+	var index int
+	err := survey.AskOne(&survey.Select{
+		Message: msg,
+		Options: options,
+	}, &index, survey.WithValidator(survey.Required))
+	if err == terminal.InterruptErr {
+		err = ErrCanceled
+	}
+	return index, err
+}
+
+// SelectString asks the user to select on of many options. It returns the entry in options with the chosen index.
+// It panics if the length of displayOptions differs from the length of options.
+func SelectString(msg string, displayOptions []string, options []string) (string, error) {
+	if len(displayOptions) != len(options) {
+		panic("Lengths of displayOptions and options don't match")
+	}
+	var index int
+	err := survey.AskOne(&survey.Select{
+		Message: msg,
+		Options: options,
+	}, &index, survey.WithValidator(survey.Required))
+	if err == terminal.InterruptErr {
+		err = ErrCanceled
+	}
+	return options[index], err
+}
---
+++ output.go
@@ -0,0 +1,150 @@
+package cli
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/mattn/go-colorable"
+)
+
+type Color string
+
+const (
+	Reset       Color = "\x1b[0m"
+	Black       Color = "\x1b[30m"
+	Red         Color = "\x1b[31m"
+	Green       Color = "\x1b[32m"
+	Yellow      Color = "\x1b[33m"
+	Blue        Color = "\x1b[34m"
+	Magenta     Color = "\x1b[35m"
+	Cyan        Color = "\x1b[36m"
+	White       Color = "\x1b[37m"
+	BlackBold   Color = "\x1b[1;30m"
+	RedBold     Color = "\x1b[1;31m"
+	GreenBold   Color = "\x1b[1;32m"
+	YellowBold  Color = "\x1b[1;33m"
+	BlueBold    Color = "\x1b[1;34m"
+	MagentaBold Color = "\x1b[1;35m"
+	CyanBold    Color = "\x1b[1;36m"
+	WhiteBold   Color = "\x1b[1;37m"
+)
+
+var out = colorable.NewColorableStdout()
+var progressStart time.Time
+var progressMsg string
+var loadingTicker *time.Ticker
+var progressBarRunning bool
+
+func BeginLoading(msg string) {
+	FinishLoading()
+	progressStart = time.Now()
+	loadingTicker = time.NewTicker(time.Second / 2)
+	progressMsg = msg
+	go func() {
+		position := 0
+		for {
+			updateLoading(position)
+			position++
+			if loadingTicker == nil {
+				return
+			}
+			_, ok := <-loadingTicker.C
+			if !ok {
+				return
+			}
+		}
+	}()
+}
+
+func updateLoading(position int) {
+	symbols := []string{"|", "/", "-", "\\"}
+	fmt.Fprintf(out, "\r%s%s %s %ds%s", Cyan, symbols[position%4], progressMsg, int(time.Now().Sub(progressStart).Seconds()), Reset)
+}
+
+func CancelLoading() {
+	if loadingTicker == nil {
+		return
+	}
+	loadingTicker.Stop()
+	loadingTicker = nil
+	fmt.Fprintln(out)
+}
+
+func FinishLoading() {
+	if loadingTicker == nil {
+		return
+	}
+	loadingTicker.Stop()
+	loadingTicker = nil
+
+	totalDuration := time.Now().Sub(progressStart)
+	var durationStr string
+	if int(totalDuration.Seconds()) == 0 {
+		durationStr = fmt.Sprintf("%dms", totalDuration.Milliseconds())
+	} else {
+		durationStr = fmt.Sprintf("%ds", int(totalDuration.Seconds()))
+	}
+
+	fmt.Fprintf(out, "\r%s√ %s %s   %s\n", Green, strings.TrimSuffix(progressMsg, "..."), durationStr, Reset)
+}
+
+func BeginProgressBar(msg string) {
+	progressMsg = msg
+	progressStart = time.Now()
+	fmt.Fprintf(out, "%s%s%s\n", Cyan, msg, Reset)
+	UpdateProgressBar(0)
+}
+
+func UpdateProgressBar(progress float64) {
+	progressBarRunning = true
+	stepSize := 0.03125
+	fmt.Fprintf(out, "\r%s[", Cyan)
+	for i := 0.0; i <= 1; i += stepSize {
+		if i <= progress && i+stepSize > progress && progress > 0 && progress < 1 {
+			fmt.Fprintf(out, ">")
+		} else if i <= progress && progress > 0 {
+			fmt.Fprintf(out, "=")
+		} else {
+			fmt.Fprintf(out, " ")
+		}
+	}
+	fmt.Fprintf(out, "] %d%% %ds%s", int(progress*100), int(time.Now().Sub(progressStart).Seconds()), Reset)
+}
+
+func CancelProgressBar() {
+	if progressBarRunning {
+		fmt.Fprintln(out)
+	}
+	progressBarRunning = false
+}
+
+func FinishProgressBar() {
+	if !progressBarRunning {
+		return
+	}
+	fmt.Fprintf(out, "\x1b[1F%s%s%s\n", Green, strings.TrimSuffix(progressMsg, "...")+"   ", Reset)
+	fmt.Fprintf(out, "%s[=================================] 100%% %ds%s\n", Green, int(time.Now().Sub(progressStart).Seconds()), Reset)
+	progressBarRunning = false
+}
+
+func Print(format string, a ...any) {
+	fmt.Fprintf(out, "%s\n", fmt.Sprintf(format, a...))
+}
+
+func PrintColor(color Color, format string, a ...any) {
+	fmt.Fprintf(out, "%s%s%s\n", color, fmt.Sprintf(format, a...), Reset)
+}
+
+func Success(format string, a ...any) {
+	PrintColor(Green, format, a...)
+}
+
+func Warn(format string, a ...any) {
+	PrintColor(Yellow, fmt.Sprintf("WARNING: "+format, a...))
+}
+
+func Error(format string, a ...any) error {
+	PrintColor(RedBold, "ERROR: "+format, a...)
+	return fmt.Errorf(format, a...)
+}
