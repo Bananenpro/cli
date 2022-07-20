c39c1d68cfd83eb557b3d4549da97e31671dce40
Julian Hofmann <julian.hofmann2805@gmail.com>
2022-07-20 17:42:17 +0200

    Fix loading not canceled on print

--- output.go
+++ output.go
@@ -129,13 +129,13 @@ func FinishProgressBar() {
 }
 
 func Print(format string, a ...any) {
-	CancelProgressBar()
+	CancelLoading()
 	CancelProgressBar()
 	fmt.Fprintf(out, "%s\n", fmt.Sprintf(format, a...))
 }
 
 func PrintColor(color Color, format string, a ...any) {
-	CancelProgressBar()
+	CancelLoading()
 	CancelProgressBar()
 	fmt.Fprintf(out, "%s%s%s\n", color, fmt.Sprintf(format, a...), Reset)
 }
